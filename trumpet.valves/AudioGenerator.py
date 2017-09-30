5bbb6dc1cf81f81ca3b8e06b0ac5910e3ef24988
aarona95 <aarona95@unet.univie.ac.at>
2017-09-30 16:05:12 +0200

    print length of final mp3

--- trumpet.valves/AudioGenerator.py
+++ trumpet.valves/AudioGenerator.py
@@ -1,19 +1,22 @@
 import os
+from mutagen.mp3 import MP3
 
 
 def generate_audio(generated_voice, best_background_sample, output_path):
-
-
-    #lower_volume = "ffmpeg -y -i " + best_background_sample + " -af volume=-15dB audiodata/lowervolume.mp3"
-    #print(lower_volume)
+    # lower_volume = "ffmpeg -y -i " + best_background_sample + " -af volume=-15dB audiodata/lowervolume.mp3"
+    # print(lower_volume)
 
     normalize = "ffmpeg -y -i " + best_background_sample + " -filter:a loudnorm audiodata/normalize.mp3"
-    #print(normalize)
+    # print(normalize)
     overlay = "ffmpeg -y -i audiodata/normalize.mp3 -i " + generated_voice + " -filter_complex amerge -ac 2 -c:a libmp3lame -q:a 4 " + output_path
 
     os.system(normalize)
     os.system(overlay)
 
+    audio = MP3("../audiodata/out.mp3")
+    print(audio.info.length)
 
-    #print(overlay)
+
+    # print(overlay)
+
 
--- trumpet.valves/VoiceGenerator.py
+++ trumpet.valves/VoiceGenerator.py
@@ -1,16 +1,13 @@
 from typing import List
 
 
-
 def call_say_command(voice_snippets: List[VoiceSnippet]) -> str:
     # TODO Call console: say -v Alex -r 100 "blablabla." -o audiodata/out-xxx.aiff
 
     for i in range(len(voice_snippets)):
-
         voice_snippet = voice_snippets[i]
 
         saycommand = 'say -v {0} -r 100 "{1}" -o audiodata/{2}.aiff'.format(voice_snippet)
-
 
 
 if __name__ == "__main__":
@@ -20,4 +17,4 @@ if __name__ == "__main__":
 
 
 
-ffmpeg -i 1.mp4 -i 2.mp4 -i 3.mp4 -i 4.mp4 -i 5.mp4 -i 6.mp4 -i 7.mp4 -i 8.mp4 -i 9.mp4 -i 10.mp4  -filter_complex '[0:0][1:0]concat=n=10:v=0:a=1[out]'  -map '[out]' -strict -2 -y 10_final.mp4
+    # ffmpeg -i 1.mp4 -i 2.mp4 -i 3.mp4 -i 4.mp4 -i 5.mp4 -i 6.mp4 -i 7.mp4 -i 8.mp4 -i 9.mp4 -i 10.mp4  -filter_complex '[0:0][1:0]concat=n=10:v=0:a=1[out]'  -map '[out]' -strict -2 -y 10_final.mp4
