2e0dd116f7c7aea5524d78ed549c94b83e11f41b
Alexander Pacha <alexander.pacha@gmail.com>
2017-09-30 14:01:59 +0200

    Added files for voice and background selection and added requirements that specify required python packages for running the code

---
+++ trumpet.valves/BackgroundSelector.py
@@ -0,0 +1,7 @@
+from typing import List
+
+import TwitterSnippet
+
+
+def select_best_background(twitter_snippets: List[TwitterSnippet]) -> str:
+    pass
---
+++ trumpet.valves/VoiceSelector.py
@@ -0,0 +1,7 @@
+from typing import List, Tuple
+
+import TwitterSnippet
+
+
+def select_best_voice(twitter_snippets: List[TwitterSnippet]) -> List[Tuple[str, str]]:
+    pass
