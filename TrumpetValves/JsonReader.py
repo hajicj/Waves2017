80df27c8061787aa5db97fa88c985edca072827d
Alexander Pacha <alexander.pacha@gmail.com>
2017-09-30 12:31:50 +0200

    Playing with json reader

--- TrumpetValves/JsonReader.py
+++ TrumpetValves/JsonReader.py
@@ -1,5 +1,10 @@
 import json
+from argparse import Namespace
+from json import JSONDecoder
+
+import jsonpickle
 
 with open("Test.json") as data_file:
-    twitter_snippets = json.load(data_file)
+    json_data = json.load(data_file)
+    twitter_snippets = jsonpickle.decode(data_file)
 print(twitter_snippets)
