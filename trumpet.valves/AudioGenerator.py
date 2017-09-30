f387b8bbe61556ffc63964e3a74a3ba477c6fa50
aarona95 <aarona95@unet.univie.ac.at>
2017-09-30 14:54:09 +0200

    audiogenerator test v2

--- trumpet.valves/AudioGenerator.py
+++ trumpet.valves/AudioGenerator.py
@@ -1,23 +1,17 @@
 import os
 
 
-def generate_audio(voice_path, background_path):
+def generate_audio(generated_voice, best_background_sample, output_path):
 
-    output_path = ""
 
-    lower_volume = "ffmpeg -i " + background_path + " -af volume=-15dB " + output_path
-    overlay = "ffmpeg -i " + output_path + " -i " + voice_path + "-filter_complex amerge -ac 2 -c:a libmp3lame -q:a 4 output.mp3"
+    lower_volume = "ffmpeg -i " + best_background_sample + " -af volume=-15dB " + output_path
+    overlay = "ffmpeg -i " + output_path + " -i " + generated_voice + "-filter_complex amerge -ac 2 -c:a libmp3lame -q:a 4 " + output_path
 
     os.system(lower_volume)
     os.system(overlay)
 
 
-    return output_path
-
-
 
 
 if __name__ == '__main__':
-    output = generate_audio("audiodata/testpeaker.aiff", "audiodata/test_sample.mp3")
-
-    print("Output: " + output)
+    generate_audio("audiodata/testpeaker.aiff", "audiodata/test_sample.mp3", "/out.mp3")
