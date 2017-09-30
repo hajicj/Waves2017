f4706e140f0c7c56d9757d228200f4493eeff627
Alexander Pacha <alexander.pacha@gmail.com>
2017-09-30 13:18:08 +0200

    First successful attempt of desersializing json

--- trumpet.valves/JsonReader.py
+++ trumpet.valves/JsonReader.py
@@ -4,7 +4,24 @@ from json import JSONDecoder
 
 import jsonpickle
 
-with open("Test.json") as data_file:
+from TwitterSnippet import TwitterSnippet, Keyword, Voice, BackgroundSample
+
+json_path = "Test.json"
+with open(json_path) as data_file:
     json_data = json.load(data_file)
-    twitter_snippets = jsonpickle.decode(data_file)
+    json_string = str(json_data).replace("'", '"')
+    twitter_snippets = jsonpickle.decode(json_string)
 print(twitter_snippets)
+
+
+class Thing(object):
+    def __init__(self, name):
+        self.name = name
+
+obj = Thing('Awesome')
+frozen = jsonpickle.encode(obj)
+thawed = jsonpickle.decode(frozen)
+
+twitter_snippet = TwitterSnippet(0, 3, "This is a tweet", 0.9, [Keyword("tweet", 0.6, [0.5, 0.1, 0.2])], [Voice("Susanne", 0.3), Voice("Hannes", 0.6)], [BackgroundSample("aggressive_mood.mp3", 0.3)])
+frozen = jsonpickle.encode(twitter_snippet)
+thawed = jsonpickle.decode(frozen)
