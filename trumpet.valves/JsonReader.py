bd5f32f2877d6f3692fb03e765d81afe4daf5991
Alexander Pacha <alexander.pacha@gmail.com>
2017-09-30 13:25:59 +0200

    Removed unused code

--- trumpet.valves/JsonReader.py
+++ trumpet.valves/JsonReader.py
@@ -13,15 +13,8 @@ with open(json_path) as data_file:
     twitter_snippets = jsonpickle.decode(json_string)
 print(twitter_snippets)
 
-
-class Thing(object):
-    def __init__(self, name):
-        self.name = name
-
-obj = Thing('Awesome')
-frozen = jsonpickle.encode(obj)
-thawed = jsonpickle.decode(frozen)
-
-twitter_snippet = TwitterSnippet(0, 3, "This is a tweet", 0.9, [Keyword("tweet", 0.6, [0.5, 0.1, 0.2])], [Voice("Susanne", 0.3), Voice("Hannes", 0.6)], [BackgroundSample("aggressive_mood.mp3", 0.3)])
-frozen = jsonpickle.encode(twitter_snippet)
-thawed = jsonpickle.decode(frozen)
+# twitter_snippet = TwitterSnippet(0, 3, "This is a tweet", 0.9, [Keyword("tweet", 0.6, [0.5, 0.1, 0.2])],
+#                                  [Voice("Susanne", 0.3), Voice("Hannes", 0.6)],
+#                                  [BackgroundSample("aggressive_mood.mp3", 0.3)])
+# frozen = jsonpickle.encode(twitter_snippet)
+# thawed = jsonpickle.decode(frozen)
