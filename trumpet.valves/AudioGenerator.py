7e5dfff28fe0b3122243d446a39277093004c326
Alexander Pacha <alexander.pacha@gmail.com>
2017-09-30 16:18:58 +0200

    Using absolute paths in AudioGenerator

--- trumpet.valves/AudioGenerator.py
+++ trumpet.valves/AudioGenerator.py
@@ -6,17 +6,22 @@ def generate_audio(generated_voice, best_background_sample, output_path):
     # lower_volume = "ffmpeg -y -i " + best_background_sample + " -af volume=-15dB audiodata/lowervolume.mp3"
     # print(lower_volume)
 
-    normalize = "ffmpeg -y -i " + best_background_sample + " -filter:a loudnorm audiodata/normalize.mp3"
-    # print(normalize)
-    overlay = "ffmpeg -y -i audiodata/normalize.mp3 -i " + generated_voice + " -filter_complex amerge -ac 2 -c:a libmp3lame -q:a 4 " + output_path
+    normalize = "ffmpeg -y -i " + os.path.abspath(best_background_sample) + " -filter:a loudnorm " + os.path.abspath(
+        "audiodata/normalize.mp3")
 
+    overlay = "ffmpeg -y -i " + os.path.abspath(
+        "audiodata/normalize.mp3") + " -i " + os.path.abspath(
+        generated_voice) + " -filter_complex amerge -ac 2 -c:a libmp3lame -q:a 4 " + \
+              os.path.abspath(output_path)
+
+    print("Executing: " + normalize)
     os.system(normalize)
+    print("Executing: " + overlay)
     os.system(overlay)
 
     audio = MP3("../audiodata/out.mp3")
     print(audio.info.length)
 
 
-    # print(overlay)
-
-
+if __name__ == "__main__":
+    generate_audio("voice1.mp3", "background.mp3", "test.mp3")
