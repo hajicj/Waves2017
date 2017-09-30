beb1cbdfa6c40e61347748e1fafe0793763e01c3
Alexander Pacha <alexander.pacha@gmail.com>
2017-09-30 14:57:54 +0200

    Added load_twitter_snippets_from_json to directly load the json instead of loading from a file

--- trumpet.valves/Valves.py
+++ trumpet.valves/Valves.py
@@ -1,5 +1,6 @@
 import argparse
 
+import AudioGenerator
 import BackgroundSelector
 import JsonReader
 import VoiceGenerator
@@ -7,18 +8,18 @@ import VoiceSelector
 from TwitterSnippet import TwitterSnippet, Voice, BackgroundSample, Keyword
 
 
-def process_request(json_path: str, output_path: str) -> None:
-    twitter_snippets = JsonReader.load_twitter_snippets_from_json(json_path)
+def process_request(json: str, output_path: str) -> None:
+    twitter_snippets = JsonReader.load_twitter_snippets_from_json(json)
     best_background_sample = BackgroundSelector.select_best_background(twitter_snippets)
     voice_snippets = VoiceSelector.select_best_voice(twitter_snippets)
     generated_voice = VoiceGenerator.call_say_command(voice_snippets)
-    # generated_sample = AudioGenerator.generate_audio(generated_voice, best_background_sample)
+    AudioGenerator.generate_audio(generated_voice, best_background_sample, output_path)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--json_path", type=str, default="Test.json",
-                        help="Path to the json-file that contains the description")
+    parser.add_argument("--json", type=str, default="Test.json",
+                        help="The JSON content to be parsed that must contain a list of ")
     parser.add_argument("--output_path", type=str, default="Output.mp3",
                         help="Path to the mp3-file that will be generated")
 
