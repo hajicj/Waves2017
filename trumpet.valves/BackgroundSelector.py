2d38b1ec465bf078c1f70d72d0b707655f1656cd
Alexander Pacha <alexander.pacha@gmail.com>
2017-09-30 14:25:48 +0200

    Implemented best-background soundtrack selector

--- trumpet.valves/BackgroundSelector.py
+++ trumpet.valves/BackgroundSelector.py
@@ -1,7 +1,24 @@
 from typing import List
 
-import TwitterSnippet
+from TwitterSnippet import TwitterSnippet, Keyword, Voice, BackgroundSample
 
 
 def select_best_background(twitter_snippets: List[TwitterSnippet]) -> str:
-    pass
+    best_sample = None
+    highest_similarity = 0
+    for twitter_snippet in twitter_snippets:
+        for background_sample in twitter_snippet.background_samples:
+            if background_sample.similarity > highest_similarity:
+                best_sample = background_sample.filename
+                highest_similarity = background_sample.similarity
+
+    return best_sample
+
+
+if __name__ == "__main__":
+    twitter_snippet = TwitterSnippet(0, 3, "This is a tweet", 0.9, [Keyword("tweet", 0.6, [0.5, 0.1, 0.2])],
+                                     [Voice("Susanne", 0.3), Voice("Hannes", 0.6)],
+                                     [BackgroundSample("aggressive_mood.mp3", 0.3)])
+
+    background_sample = select_best_background([twitter_snippet])
+    print("Best background sample: {0}".format(background_sample))
